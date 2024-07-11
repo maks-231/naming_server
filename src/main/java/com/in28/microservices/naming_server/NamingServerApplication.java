407b596bc33005bfb520884a6897f011e101e8c6
mahtirschii <mahtirschii@amsoft-group.com>
2024-07-11 16:34:51 +0300

    initial commit

---
+++ src/main/java/com/in28/microservices/naming_server/NamingServerApplication.java
@@ -0,0 +1,14 @@
+package com.in28.microservices.naming_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+}
